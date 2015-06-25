5840067079db9c57e6e6e32bb47b066a028ce1aa
Lee SungJu <lsj4401@zuminternet.com>
2015-06-25 13:16:52 +0900

    맵에 초기 돌배치 로직 추가

--- Life_Game/src/com/life/manager/GameManager.java
+++ Life_Game/src/com/life/manager/GameManager.java
@@ -7,19 +7,31 @@ import com.life.game.rule.CopyStrategyRule;
 import com.life.viewer.ConsoleViewer;
 import com.life.viewer.Viewer;
 
-public class GameManager implements Runnable{
+public class GameManager implements Runnable {
 	private final long INTERVAL = 5000L;
 	private final int X_LENGTH = 10;
 	private final int Y_LENGTH = 10;
 	
 	private boolean isPlay = false;
 	
-	private AbstractRule rule = new CopyStrategyRule();
+	private AbstractRule rule;
 	private LifeMap map = new ArrayLifeMap(X_LENGTH,Y_LENGTH);
 	private Viewer viewer = new ConsoleViewer();
 	
+	public GameManager() {
+		map.alive(4, 3);
+		map.alive(5, 3);
+		map.alive(6, 3);
+		
+		map.alive(4, 4);
+		map.alive(4, 5);
+		
+		rule = new CopyStrategyRule(map);
+	}
+	
 	public void play(){
 		new Thread(this).start();
+		isPlay = true;
 	}
 	
 	public void stopPlay(){
@@ -31,7 +43,7 @@ public class GameManager implements Runnable{
 		// TODO Auto-generated method stub
 		while(isPlay){
 			try {
-				this.rule.nextState();
+				map = rule.nextState();
 				Thread.sleep(INTERVAL);
 				viewer.draw(map);
 			} catch (Exception e) {
