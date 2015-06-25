86b78a2e33849b10def2ef965c3f77e001082d8a
Lee SungJu <lsj4401@zuminternet.com>
2015-06-25 13:17:15 +0900

    게임실행 로직 추가

--- Life_Game/src/com/life/client/Main.java
+++ Life_Game/src/com/life/client/Main.java
@@ -1,10 +1,12 @@
 package com.life.client;
 
+import com.life.manager.GameManager;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		GameManager m = new GameManager();
+		m.play();
 	}
 
 }
